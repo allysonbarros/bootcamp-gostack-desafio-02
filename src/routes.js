5a3ef23cc0c84748a1ae3d4c0d75e54c512f907b
Allyson Barros <allysonbarrosrn@gmail.com>
2019-10-16 20:11:26 -0300

    Finalizando o desafio 02.

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,19 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import StudentsController from './app/controllers/StudentsController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.post('/students', StudentsController.store);
+routes.put('/students/:id', StudentsController.update);
 
 export default routes;
