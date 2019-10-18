e645f5688a4e7b873139884d3e835a029baebde1
Allyson Barros <allysonbarrosrn@gmail.com>
2019-10-18 09:57:00 -0300

    Criando a funcionalidade de envio de email.

---
+++ src/app/jobs/RegistrationMail.js
@@ -0,0 +1,39 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import Mail from '../../lib/Mail';
+
+class RegistrationMail {
+  get key() {
+    return 'RegistrationMail';
+  }
+
+  async handle({ data }) {
+    const { registration } = data;
+
+    await Mail.senddMail({
+      to: `${registration.student.name} <${registration.student.email}>`,
+      subject: 'Matrícula realizada! [GymPoint]',
+      template: 'registration',
+      context: {
+        student: registration.student,
+        registration,
+        start_date: format(
+          parseISO(registration.start_date),
+          "dd 'de' MMMM 'de' YYYY.'",
+          {
+            locale: pt,
+          }
+        ),
+        end_date: format(
+          parseISO(registration.end_date),
+          "dd 'de' MMMM 'de' YYYY.'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new RegistrationMail();
