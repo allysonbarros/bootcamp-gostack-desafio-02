95c84b272a76144542c00dd0b5e106010f441715
Allyson Barros <allysonbarrosrn@gmail.com>
2019-11-11 20:46:33 -0300

    Configurando o projeto web.

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+import SignIn from '~/pages/SignIn';
+import Students from '~/pages/Students';
+import Plans from '~/pages/Plans';
+import Registrations from '~/pages/Registrations';
+import HelpOrders from '~/pages/HelpOrders';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/students" component={Students} isPrivate />
+      <Route path="/plans" component={Plans} isPrivate />
+      <Route path="/registrations" component={Registrations} isPrivate />
+      <Route path="/help_orders" component={HelpOrders} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
+    </Switch>
+  );
+}
