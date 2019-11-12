6ed8ff79d0663c5a51c05d693065f3652ac1efdd
Allyson Barros <allysonbarrosrn@gmail.com>
2019-11-12 19:20:54 -0300

    Adicionando o unform nos formulários de autenticação e recuperação de senha.

--- frontend/src/pages/ForgotPassword/index.js
+++ frontend/src/pages/ForgotPassword/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo.svg';
 
-import {} from './styles';
-
 export default function ForgotPassword() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GymPoint" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label htmlFor="email">
           Seu E-mail
-          <input type="email" name="email" placeholder="exemplo@email.com" />
+          <Input type="email" name="email" placeholder="exemplo@email.com" />
         </label>
 
         <button type="submit">Recuperar a Senha</button>
         <Link to="/">Cancelar</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GymPoint" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label htmlFor="email">
           Seu E-mail
-          <input type="email" name="email" placeholder="exemplo@email.com" />
+          <Input type="email" name="email" placeholder="exemplo@email.com" />
         </label>
         <label htmlFor="password">
           Sua Senha
-          <input
+          <Input
             type="password"
             name="password"
             placeholder="Sua senha secreta"
@@ -24,7 +29,7 @@ export default function SignIn() {
 
         <button type="submit">Entrar no sistema</button>
         <Link to="/forgot-password">Esqueci minha Senha</Link>
-      </form>
+      </Form>
     </>
   );
 }
